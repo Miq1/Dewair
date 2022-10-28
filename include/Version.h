21e761146e89c377bbcc4345f76b9e8ea1511212
Miq1 <miq1@gmx.de>
2022-10-28 17:55:14 +0200

    Update .gitignore

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.58"
+        #define VERSION "0.1.59"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-10-27 18:11:23.937914"
+        #define BUILD_TIMESTAMP "2022-10-28 17:19:36.653646"
     #endif
     
