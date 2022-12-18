57b398ed92f102d00e04a1d467d5dd68fb5691a1
Miq1 <miq1@gmx.de>
2022-12-18 18:07:31 +0100

    Add Fallback Modbus register docs

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.88"
+        #define VERSION "0.1.90"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-12-18 17:36:00.453100"
+        #define BUILD_TIMESTAMP "2022-12-18 18:04:21.514559"
     #endif
     
