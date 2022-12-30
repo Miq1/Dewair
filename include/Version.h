d465095d662dd03dcbbd1423bc15a627970580c6
Miq1 <miq1@gmx.de>
2022-12-30 15:35:27 +0100

    Add case files

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.91"
+        #define VERSION "0.1.92"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-12-25 10:40:29.309095"
+        #define BUILD_TIMESTAMP "2022-12-30 15:22:09.983983"
     #endif
     
