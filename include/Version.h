428ca9c9409f8dd2b229085a815692c22b6c4da4
Miq1 <miq1@gmx.de>
2023-01-27 15:51:35 +0100

    Crank up version to 1.0

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.111"
+        #define VERSION "1.0.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-01-24 10:02:30.903760"
+        #define BUILD_TIMESTAMP "2023-01-27 15:48:31.346224"
     #endif
     
