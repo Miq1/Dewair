d3c8185da10589f233d92b3ae76c7436875abdd0
Miq1 <miq1@gmx.de>
2023-03-02 16:17:05 +0100

    Add slot marker to diagram

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.0.42"
+        #define VERSION "1.0.43"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-03-01 16:12:48.134856"
+        #define BUILD_TIMESTAMP "2023-03-02 16:00:59.390259"
     #endif
     
